f97c1d60ea4983226b09130d4a9854f405d94870
Will Brennan <willfbren@gmail.com>
2020-02-24 19:06:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,10 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.legnth.times do |count|
+    if array[count] == value_to_find
+      return count
+    end
+    nil 
+  end
 end
 
 def find_max_value(array)
